dee4dfb5f89a7cffc1f08bca39e346d7733760b4
dziudek <dziudek@gmail.com>
2013-09-05 11:34:03 +0200

    Added margin support

--- mod_grid_gk5/tmpl/default.php
+++ mod_grid_gk5/tmpl/default.php
@@ -22,10 +22,12 @@ defined('_JEXEC') or die('Restricted access');
 	data-random="<?php echo $this->config['animation_random']; ?>"
 	data-speed="<?php echo $this->config['animation_speed']; ?>"
 	data-type="<?php echo $this->config['animation_type']; ?>"
->                
-	<?php $this->moduleRender(); ?>
-	 
-	<img class="gkImgDesktop" src="data:image/png;base64,<?php echo $this->generateBlankImage(60, 10 * $this->config['grid_data']->heights->desktop); ?>" alt="" />
-	<img class="gkImgTablet" src="data:image/png;base64,<?php echo $this->generateBlankImage(40, 10 * $this->config['grid_data']->heights->tablet); ?>" alt="" />
-	<img class="gkImgMobile" src="data:image/png;base64,<?php echo $this->generateBlankImage(20, 10 * $this->config['grid_data']->heights->mobile); ?>" alt="" />
+>    
+	<div class="gkGridGK5Wrap" style="margin: <?php echo $this->config['grid_margin']; ?>">            
+		<?php $this->moduleRender(); ?>
+		 
+		<img class="gkImgDesktop" src="data:image/png;base64,<?php echo $this->generateBlankImage(60, 10 * $this->config['grid_data']->heights->desktop); ?>" alt="" />
+		<img class="gkImgTablet" src="data:image/png;base64,<?php echo $this->generateBlankImage(40, 10 * $this->config['grid_data']->heights->tablet); ?>" alt="" />
+		<img class="gkImgMobile" src="data:image/png;base64,<?php echo $this->generateBlankImage(20, 10 * $this->config['grid_data']->heights->mobile); ?>" alt="" />
+	</div>
 </div>
